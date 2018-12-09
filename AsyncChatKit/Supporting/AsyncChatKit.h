9b5ce027322e00aa0a68c4f5c7a4961da416d87f
Murodjon Karimov <xxxakkiii@gmail.com>
2018-12-09 17:04:03 +0500

    init

---
+++ AsyncChatKit/Supporting/AsyncChatKit.h
@@ -0,0 +1,19 @@
+//
+//  AsyncChatKit.h
+//  AsyncChatKit
+//
+//  Created by Murodjon Karimov on 12/9/18.
+//  Copyright © 2018 KMITM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncChatKit.
+FOUNDATION_EXPORT double AsyncChatKitVersionNumber;
+
+//! Project version string for AsyncChatKit.
+FOUNDATION_EXPORT const unsigned char AsyncChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncChatKit/PublicHeader.h>
+
+
